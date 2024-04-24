a9e5804aa089980e1bf94e619a1be6deb0b07eaa
tonmoy-Og <tonmoy15-5200@diu.edu.bd>
2024-04-24 15:29:52 +0600

    added theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,14 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+    darkTheme: "light",
+    base: true, 
+    styled: true,
+    utils: true,
+    rtl: false,
+    prefix: "",
+    logs: true,
+  },
 }
